369b52b364e641500b897cdc0319fb01ba288e08
Anton Kapylets <AntonKopilec@gmail.com>
2016-04-09 12:25:15 +0200

    add custom select for categories

---
+++ 1st task/categoryController.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class CategoryCtrl {
+	constructor(view) {
+		this.view = document.querySelector(view);
+		this.bindObject();
+		this.events();
+	};
+	get __objects () {
+		return {
+			controls:'.controls',
+			input:'.input',
+			categories:'.categories'
+		}
+	};
+	get options () {
+		return {
+			category: '.category'
+		}
+	};
+	bindObject () {
+		this.objects = {};
+		for(let object in this.__objects) {
+			let selector = this.__objects[object];
+			this.objects[object] = this.view.querySelector(selector);
+		}
+	};
+	toggleShow (event) {
+		let categories = this.objects.categories;
+		if(categories.classList.contains('categories-show')){
+			categories.classList.remove('categories-show');
+		}
+		else{
+			categories.classList.add('categories-show');
+		}
+	};
+	changeCategory ({target}) {
+		if(!target.classList.contains('category')){
+			return;
+		}
+		if(target.innerHTML === this.objects.input.innerHTML){
+			return;
+		}
+		this.objects.input.innerHTML = target.innerHTML;
+	}
+	events () {
+		var self = this;
+		this.objects.controls.addEventListener('click',this.toggleShow.bind(this));
+		this.objects.categories.addEventListener('click',this.changeCategory.bind(this));
+	}
+
+}
---
+++ 1st task/script.js
@@ -0,0 +1,3 @@
+
+
+var catCtrl = new CategoryCtrl('#category-panel');
