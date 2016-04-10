ec6be99aafbbbde9c4dc914ba1e73796b49e9650
Kapylets Anton <Kapylets Anton>
2016-04-10 14:10:51 +0200

    add news viewer

--- 1st task/script.js
+++ 1st task/script.js
@@ -3,3 +3,6 @@ var Globals = Globals || {};
 Globals.observer = new Observer();
 
 var catCtrl = new CategoryCtrl('#category-panel');
+
+var newsTemplate = new NewsTemplate();
+var newReader = new NewsReader('#news',newsTemplate);
